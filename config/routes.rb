ca9abc82f0b96ab35881db334d9faf8e708ce19a
Maria <unamaria@gmail.com>
2015-09-12 15:31:00 -0500

    Delete help

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
 
   root            		 'static_pages#home'
-  get 'help'   		 	=> 'static_pages#help'
   get 'about' 		  => 'static_pages#about'
   get 'contact' 		=> 'static_pages#contact'
   get 'signup'  		=> 'users#new'
