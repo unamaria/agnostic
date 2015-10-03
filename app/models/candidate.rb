2f4bba15d12d04d12f81280adf070e59db2944aa
Maria <unamaria@gmail.com>
2015-10-03 12:59:25 -0500

    Format resume raw

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -7,31 +7,32 @@ class Candidate < ActiveRecord::Base
   validates_attachment :resume_pdf, :content_type => { :content_type => "application/pdf" }
 
 	before_create :create_nickname
+	# before_create :create_resume_raw input must be an IO-like object or a filename
+	# before_update :create_resume_raw RECURSIVE
 
-	def create_resume_raw
-		reader = PDF::Reader.new(resume_pdf.path)
-		lines = reader.pages[0].text.lines.map(&:chomp)
-		lines.slice!(0)
-		# lines = array_to_html(lines)
-		update_attribute(:resume_raw, lines)
-	end
 
-	def pdf_remove_first_line
-		resume_raw.slice!(0)
-	end
-
+		def create_resume_raw
+			reader = PDF::Reader.new(resume_pdf.path)
+			lines = reader.pages[0].text.lines.map(&:chomp)
+			lines.slice!(0)
+			lines_html = to_html(lines)
+			update_attribute(:resume_raw, lines_html)
+		end
 	private
 
 		def create_nickname
 			self.nickname = "Pomegranade #{self.id}"
 		end
 
-		# def array_to_html(array)
-		# 	array = array.split(",")
-		# 	array.each do |value|
-		# 		value = "<p>#{value}</p>"
-		# 	end
-		# 	if array[0] == "<p></p>" then array.slice!(0) end
-		# 	array.join(",")
-		# end
+		def to_html(lines)
+			headings = %w(education experience projects languages skills)
+			lines.each_with_index do |value, index| 
+				if headings.include?(value.downcase)
+					lines[index] = "<strong>#{value}</strong>" 
+				end
+			end
+			lines_html = lines.map { |value| value = "<p>#{value}</p>" }
+			lines_html = lines_html.keep_if { |value| value != "<p></p>" }
+			lines_html.join("")
+		end
 end
